dbd116761673a21e1e2e63a5ea142e66061557c5
ankit <ak248100@gmail.com>
2020-12-01 13:30:29 +0530

    'chromedriver'

--- wastatus/tasks.py
+++ wastatus/tasks.py
@@ -2,6 +2,7 @@ from background_task import background
 import time
 
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,16 +12,16 @@ import time
 import sys
 import os
 
-chrome_options = webdriver.ChromeOptions()
+chrome_options = webdriver.FirefoxOptions()
 # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 # chrome_options.add_argument("user-agent=User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36")
 chrome_options.add_argument("--headless")
 # chrome_options.add_argument("--window-size=1280x722")
-chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
 # driver = webdriver.Chrome(r'C:\Users\Ankit\Documents\Django\projects\wastatus\wastatus\chromedriver.exe',options=chrome_options)
-driver = webdriver.Chrome(executable_path=r"/usr/local/lib/chromedriver",options=chrome_options)
+driver = webdriver.Firefox(executable_path=r"/usr/local/lib/geckodriver",options=chrome_options)
 
 driver.get('https://web.whatsapp.com')
 
