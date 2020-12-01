629254775baaf6a24a9f80733441b8ff0468d6b5
ankit <ak248100@gmail.com>
2020-12-01 14:43:28 +0530

    'chromedriver'

--- wastatus/tasks.py
+++ wastatus/tasks.py
@@ -11,16 +11,16 @@ import time
 import sys
 import os
 
-chrome_options = webdriver.ChromeOptions()
+options = webdriver.ChromeOptions()
 # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 # chrome_options.add_argument("user-agent=User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36")
-chrome_options.add_argument("--headless")
+options.add_argument("--headless")
 # chrome_options.add_argument("--window-size=1280x722")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
 
 # driver = webdriver.Chrome(r'C:\Users\Ankit\Documents\Django\projects\wastatus\wastatus\chromedriver.exe',options=chrome_options)
-driver = webdriver.Chrome(executable_path=r"/usr/local/lib/chromedriver",service_log_path='~/wastatus/wastatus',options=chrome_options)
+driver = webdriver.Chrome('chromedriver',options=options)
 
 driver.get('https://web.whatsapp.com')
 
