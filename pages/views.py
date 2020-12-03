a5404142c0b8ba9a6b0aa4b4192aaa247c874fb8
ankit <ak248100@gmail.com>
2020-12-03 19:19:29 +0530

    'file'

--- pages/views.py
+++ pages/views.py
@@ -11,7 +11,7 @@ def home_view(request):
     return render(request,'home.html',{})
 
 def button_view(request):
-    process_tasks_cmd = "python manage.py process_tasks"
-    process_tasks_args = shlex.split(process_tasks_cmd)
-    subprocess.Popen(process_tasks_args)
+    # process_tasks_cmd = "python manage.py process_tasks"
+    # process_tasks_args = shlex.split(process_tasks_cmd)
+    # subprocess.Popen(process_tasks_args)
     return render(request,'home.html',{})
