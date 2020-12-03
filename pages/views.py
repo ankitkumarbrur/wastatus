c63b13cb207cbbce11c2c336cf330ac0478dc832
ankit <ak248100@gmail.com>
2020-12-03 22:22:31 +0530

    'cmd'

--- pages/views.py
+++ pages/views.py
@@ -2,16 +2,15 @@ from django.shortcuts import render
 
 from wastatus.tasks import fun
 
-import subprocess
-import shlex
+# import subprocess
+# import shlex
 
+import os
 
 # Create your views here.
 def home_view(request):
     return render(request,'home.html',{})
 
 def button_view(request):
-    process_tasks_cmd = "sudo reeboot"
-    process_tasks_args = shlex.split(process_tasks_cmd)
-    subprocess.Popen(process_tasks_args)
+    os.system('sudo reboot')
     return render(request,'home.html',{})
