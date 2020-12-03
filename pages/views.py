e402dc8825d951dbabb2fe093dc42881fd6085d8
ankit <ak248100@gmail.com>
2020-12-04 01:13:55 +0530

    'file'

--- pages/views.py
+++ pages/views.py
@@ -12,5 +12,5 @@ def home_view(request):
     return render(request,'home.html',{})
 
 def button_view(request):
-    subprocess.call("source wastatus/my_env/bin/activate && python3 /home/ak248100/wastatus/manage.py process_tasks", shell=True)
+    subprocess.call("source /home/ak248100/wastatus/my_env/bin/activate && python3 /home/ak248100/wastatus/manage.py process_tasks", shell=True)
     return render(request,'home.html',{})
