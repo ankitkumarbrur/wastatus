ab260df3f8bed4383f36e5e06697de6e86b04bfe
ankit <ak248100@gmail.com>
2020-12-04 00:31:40 +0530

    'file'

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 from wastatus.tasks import fun
 
-# import subprocess
-# import shlex
+import subprocess
+import shlex
 
 import os
 
@@ -12,5 +12,6 @@ def home_view(request):
     return render(request,'home.html',{})
 
 def button_view(request):
-    os.system('python3 manage.py process_tasks')
+    pr = shlex.split("python3 manage.py process_tasks")
+    process_ta = subprocess.Popen(pr)
     return render(request,'home.html',{})
