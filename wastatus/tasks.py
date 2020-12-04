02bc22c12b0b37f7a67a9d3c004cf796c9c9475c
ankit <ak248100@gmail.com>
2020-12-05 01:48:47 +0530

    Added StaticFiles

--- wastatus/tasks.py
+++ wastatus/tasks.py
@@ -21,19 +21,13 @@ chrome_options.add_argument("--no-sandbox")
 
 driver = webdriver.Chrome(executable_path="/home/ak248100/wastatus/wastatus/chromedriver", options=chrome_options)
 
-driver.get("https://web.whatsapp.com/")
-
-driver.save_screenshot('/home/ak248100/wastatus/static/screenshot.png')
-
-driver.quit()
-
 @background(schedule = 0)
 def fun():
     print('FUN')
-    t = 0
-    file = open('/home/ak248100/wastatus/file.txt', 'a')
-    while (t <= 10):
-        t = t+1
-        file.write('\nI am from fun' + str(t))
-        time.sleep(2)
-    file.close()
+    driver.get("https://web.whatsapp.com/")
+
+    time.sleep(5)
+
+    driver.save_screenshot('/home/ak248100/wastatus/static/screenshot.png')
+
+    driver.quit()
