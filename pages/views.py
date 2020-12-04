4284895f813a9548ec69699f32c5c433fb47db11
ankit <ak248100@gmail.com>
2020-12-04 15:37:22 +0530

    'l'

--- pages/views.py
+++ pages/views.py
@@ -12,5 +12,5 @@ def home_view(request):
     return render(request,'home.html',{})
 
 def button_view(request):
-    subprocess.Popen("cd /home/ak248100/wastatus ", shell=True)
+    subprocess.Popen("source /home/ak248100/wastatus/my_env/bin/activate ", shell=True)
     return render(request,'home.html',{})
